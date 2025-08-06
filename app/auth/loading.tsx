fac99af9cfd602ecfbd42acee421ca24c7cb3105
RaNiiJohnson <johnsontolotriniavo@gmail.com>
2025-08-06 22:25:54 +0300

    setup loading page

--- app/auth/loading.tsx
+++ app/auth/loading.tsx
@@ -1,13 +1,3 @@
-import { Skeleton } from "@/components/ui/skeleton";
-
 export default function loading() {
-  return (
-    <div className="max-w-md mx-auto mt-20 p-4">
-      <div className="space-y-4">
-        <Skeleton className="h-10 w-full rounded-md" />
-        <Skeleton className="h-10 w-full rounded-md" />
-        <Skeleton className="h-10 w-1/2 rounded-md mx-auto" />
-      </div>
-    </div>
-  );
+  return <div></div>;
 }
