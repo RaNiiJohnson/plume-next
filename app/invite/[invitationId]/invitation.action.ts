fd90fb147729238c721be78b3c1789d65332cbc6
RaNiiJohnson <johnsontolotriniavo@gmail.com>
2025-08-19 15:29:36 +0300

    remove return

--- app/invite/[invitationId]/invitation.action.ts
+++ app/invite/[invitationId]/invitation.action.ts
@@ -49,7 +49,6 @@ export async function acceptInvitationAction({
         data: { status: "accepted" },
       });
       redirect(`/workspace/${invitation.organizationId}`);
-      return;
     }
 
     await auth.api.acceptInvitation({
